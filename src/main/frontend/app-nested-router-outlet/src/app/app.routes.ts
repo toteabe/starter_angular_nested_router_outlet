fe7ec2e1d61ce9c8d99c9fdf21b2c712ac8905be
toteabe <toteabe@yahoo.es>
2024-05-25 14:11:53 +0200

    reemplazando dashboard con tab

--- src/main/frontend/app-nested-router-outlet/src/app/app.routes.ts
+++ src/main/frontend/app-nested-router-outlet/src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from  './dashboard/dashboard.component'
 import {Tab1Component} from "./dashboard/tab1/tab1.component";
 import {Tab2Component} from "./dashboard/tab2/tab2.component";
-export const routes: Routes = [
+export const routes: Routes =
+    [
     {
         path: '', pathMatch: 'full', redirectTo: 'dashboard'
     },
