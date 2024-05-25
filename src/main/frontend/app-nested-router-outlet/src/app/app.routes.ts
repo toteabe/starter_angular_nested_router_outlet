de2de9b88436f6f008c32c4ad0cdc95656e681a7
toteabe <toteabe@yahoo.es>
2024-05-25 16:29:55 +0200

    detectando cambio de ruta en componente padre

--- src/main/frontend/app-nested-router-outlet/src/app/app.routes.ts
+++ src/main/frontend/app-nested-router-outlet/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from  './dashboard/dashboard.component'
 import {Tab1Component} from "./dashboard/tab1/tab1.component";
 import {Tab2Component} from "./dashboard/tab2/tab2.component";
+
+//https://blog.devgenius.io/the-art-of-nested-router-outlets-in-angular-dafb38245a30
 export const routes: Routes = [
     {
         path: '', pathMatch: 'full', redirectTo: 'dashboard'
--- src/main/frontend/app-nested-router-outlet/src/app/dashboard/dashboard.component.ts
+++ src/main/frontend/app-nested-router-outlet/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Tab1Component} from "./tab1/tab1.component";
 import {Tab2Component} from "./tab2/tab2.component";
 import {NgIf} from "@angular/common";
-import {RouterLink, RouterOutlet} from "@angular/router";
+import {ActivatedRoute, NavigationEnd, NavigationStart, Router, RouterLink, RouterOutlet} from "@angular/router";
+import {filter, map} from "rxjs";
 
 @Component({
   selector: 'app-dashboard',
@@ -17,8 +18,28 @@ import {RouterLink, RouterOutlet} from "@angular/router";
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit  {
 
   currentTab: string = 'Tab1';
+
+  //private activatedRoute = inject(ActivatedRoute);
+
+  //Utilizo inyeccion por constructor de typescript
+  constructor(p private router: Router){}
+  ngOnInit() {
+    console.log("En este instante el componente ha cargado");
+
+
+    //https://ultimatecourses.com/blog/detecting-route-changes-angular-router
+    this.router.events.pipe(
+        filter((event) => event instanceof NavigationStart)
+    ).subscribe((event) => {
+      // this only fires for `NavigationStart` and no other events
+      console.log(event);
+    });
+
+
+  }
+
 
 }
